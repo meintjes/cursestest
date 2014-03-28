72302262c439dc90b4c70f6cd76d43ce155f02ec
arxanas <arxanas@gmail.com>
2014-03-28 17:54:40 -0400

    Added the actual script because I can't actually git

---
+++ regenerateUnserializer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+find . -type f -print0 |
+xargs -0 egrep "^\s*CREATE_TAG_FOR\([a-zA-Z]+\)\s*\$" |
+cut -d '(' -f 2 |
+cut -d ')' -f 1 |
+awk '
+BEGIN {
+    print "#ifndef __GET_POINTER_FROM_ARCHIVE_H__"
+    print "#define __GET_POINTER_FROM_ARCHIVE_H__"
+    print "#include <exception>"
+    print "#include <string>"
+    print "#include \"Archive.h\""
+    print "void* getPointerFromArchive(Archive &ar) {"
+    print "  std::string tag;"
+    print "  ar & tag;"
+    print "  switch (tagize(tag.c_str())) {"
+}
+
+{
+    print "  CREATE_CASE_FOR(" $1 ")"
+}
+
+END {
+    print "  CREATE_NONE_CASE"
+    print "  default:"
+    print "    throw new std::runtime_error(\"Invalid serialization tag: \" + tag);"
+    print "    return nullptr;"
+    print "  }"
+    print "}"
+    print "#endif"
+}
+'
